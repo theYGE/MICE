fa1b11905bfbb5ce4c8760973fb7fd7c77e9a804
Alex Makarevych <a.makarevych@reply.de>
2024-08-12 17:59:13 +0200

    add logistic regression outline

---
+++ logistic_regression.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+# TODO: Define set of arguments
+def logistic_regression():
+
+    # Logistic regression is for binary variables
+
+
+    # Algorithm:
+    #     1. Fit a logistic regression model on Y observed using scikit-learn
+    #     4. For each missing Y predict Y from X and fitted model
+    #     5. Y imputed = Y predicted with highest probability
+
+
+
+    # TODO: Define return value. Imputed dataset?
+    return None
