aba4f88ba3a6320aab35e2926422686de6585ca6
Alex Makarevych <a.makarevych@reply.de>
2024-08-12 17:56:21 +0200

    add layout for multinominal logistic regression

---
+++ multinominal_logistic_regression.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+# TODO: Define set of arguments
+def mlr():
+
+    # Multinominal regression model is used for the cases when we have categorical, but not ordinal values
+
+    # Algorithm:
+    #     1. Fit a multinominal regression model on Y observed using scikit-learn
+    #     4. For each missing Y predict Y from X and fitted model
+    #     5. Y imputed = Y predicted with highest probability
+
+
+
+    # TODO: Define return value. Imputed dataset?
+    return None
